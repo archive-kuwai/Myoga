e44751f5987f76178a4899ef1ca86e71b8db34c0
naohta <naohta@gmail.com>
2012-07-31 14:02:56 +0900

    git issue. PageFile and Pagefile

---
+++ src/webapi/pages/PagefFFFFFFFileLoader.java
@@ -0,0 +1,27 @@
+package webapi.pages;
+
+import java.io.IOException;
+import java.nio.charset.*;
+import java.nio.*;
+import java.nio.file.*;
+import d.u.SimpleString;
+
+public class PagefileLoader {
+
+	public static SimpleString page2(String filePathAsServletContext) {
+		StringBuffer sb = new StringBuffer();
+
+		try {
+			// TODO 相対パスで読みたい "webapi/pages/page2.html"
+			//for(String s : Files.readAllLines(Paths.get("C:/Documents and Settings/esl09002039/git_repos/wkspace_juno_myoga/myoga/build/classes/webapi/pages/page2.html"), Charset.forName("UTF-8"))){
+			for(String s : Files.readAllLines(Paths.get(filePathAsServletContext, "WEB-INF/classes/webapi/pages/page2.html"), Charset.forName("UTF-8"))){
+			    sb.append(s);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+		return new SimpleString(sb.toString());
+	}	
+	
+}

ce3c58fcecea973f0541a7b6004cc60225cb093d
naohta <naohta@gmail.com>
2012-10-17 10:10:38 +0900

    tidy up

--- WebContent/make_selector.js
+++ WebContent/make_selector.js
@@ -1,13 +1,4 @@
-function make_selector(jq, target_function, items, labels){
-	jq.empty();
-	jq.append('<select onChange="' +target_function+ '"></select>');
-	for(var i=0; i<items.length; i++){
-		jq.find('select').append("<option value='" +items[i]+ "' id='" +items[i]+ "'>" + labels[i] + "</option>");
-	}
-}
-
-
-function make_selector2(jq, items, labels){
+function make_selector(jq, items, labels){
 	jq.empty();
 	jq.append('<select></select>');
 	for(var i=0; i<items.length; i++){

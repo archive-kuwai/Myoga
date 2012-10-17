ef7b928fdbddd28464ec05ea386618bb0d643ed6
naohta <naohta@gmail.com>
2012-10-17 10:01:59 +0900

    using jQuery's change() function.

--- WebContent/make_selector.js
+++ WebContent/make_selector.js
@@ -7,10 +7,9 @@ function make_selector(jq, target_function, items, labels){
 }
 
 
-function make_selector2(jq, target_function, items, labels){
+function make_selector2(jq, items, labels){
 	jq.empty();
 	jq.append('<select></select>');
-	jq.find('select').attr("onChange",target_function);
 	for(var i=0; i<items.length; i++){
 		jq.find('select').append("<option value='" +items[i]+ "' id='" +items[i]+ "'>" + labels[i] + "</option>");
 	}

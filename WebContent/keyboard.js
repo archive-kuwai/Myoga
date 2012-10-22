b27a2188d06c7ead2281663bcf8866b0f7d84fce
naohta <naohta@gmail.com>
2012-10-22 17:59:43 +0900

    anyway..

--- WebContent/keyboard.js
+++ WebContent/keyboard.js
@@ -1,3 +1,6 @@
+/*
+ * Allow backspace key 
+ */
 $('textarea').keydown(function(e){ /*donot include #main_content*/
 	if(e.keyCode==8){
 	  e.stopPropagation();
@@ -5,8 +8,12 @@ $('textarea').keydown(function(e){ /*donot include #main_content*/
 	}
 });
 
+
+/*
+ * Prevent backspace key & function keys (F1-F12)
+ */
 $(window).keydown(function(e){
-	//console.log("keyCode[" + e.keyCode + "] detected by $(window).");
+	console.log("keyCode[" + e.keyCode + "] detected by $(window).");
 	
 	if(		
 			e.keyCode == 8 /*backspace*/
@@ -16,3 +23,19 @@ $(window).keydown(function(e){
 		return false;
 	}
 });
+
+
+
+/*
+ * ======================================================================
+ * Allow direct alphabet
+ */
+/*
+$('.direct_alphabet').keydown(function(e){ /*donot include #main_content
+	if(65 <= e.keyCode && e.keyCode==90){
+	  $(this).val($(this).val() + String.fromCharCode(e.keyCode+32));
+	  e.stopPropagation();
+	  return true;
+	}
+});
+*/

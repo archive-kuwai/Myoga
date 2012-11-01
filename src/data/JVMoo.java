f456d67e5ee180a3cf50d536f9a97089357c6f61
naohta <naohta@gmail.com>
2012-11-01 15:07:31 +0900

    not work

---
+++ src/data/JVMoo.java
@@ -0,0 +1,18 @@
+package data;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+import net.arnx.jsonic.*;
+import com.amazonaws.*;
+import com.amazonaws.auth.*;
+import com.amazonaws.services.dynamodb.*;
+
+public class JVMoo {
+	
+	protected static SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd HH:mm (ss.SSS)");
+    protected static List history = new LinkedList();
+
+    public void save
+    
+}

1e63aca3ec17314ad3b8542db01cbb0a62490d18
naohta <naohta@gmail.com>
2012-11-05 11:16:28 +0900

    hi. anyway

--- src/webapi/command/Command.java
+++ src/webapi/command/Command.java
@@ -4,6 +4,8 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
+import org.codehaus.jackson.annotate.JsonIgnore;
+
 import data.JVMoo;
 import net.arnx.jsonic.JSON;
 import net.arnx.jsonic.JSONHint;
@@ -11,15 +13,17 @@ import net.arnx.jsonic.JSONHint;
 public class Command extends JVMoo{
 	
 	public Command() throws IOException {}
-	@JSONHint(ordinal=0) public Who who;
 	@JSONHint(ordinal=1) public Date when_receive;
 	@JSONHint(ordinal=2) public Date when_answer;
 	@JSONHint(ordinal=3) public Method method;
 
+	@JsonIgnore
 	public String getUuid(){return uuid;}
 	public void setUuid(String s){this.uuid=s;}
 	private String uuid;
 	
+	@JSONHint(ordinal=0) 
+	public Who who;
 	public String getWhenReceive(){return fmt.format(when_receive);}
 	public void setWhenReceive(String s) throws ParseException{when_receive=fmt.parse(s);}
 	

e0a00c3de04f94317cec896ad67e09add51a765b
naohta <naohta@gmail.com>
2012-11-06 12:59:29 +0900

    end of day

--- WebContent/samples/keyboard.js
+++ WebContent/samples/keyboard.js
@@ -12,7 +12,7 @@ $(':password, :text, textarea').keydown(function(e){
 /*
  * Prevent backspace key & function keys (F1-F12)
  */
-$(window).keydown(function(e){
+$(window).keydown(function(e,sender){
 	if(		
 			e.keyCode == 8 /*backspace*/
 			|| (112<=e.keyCode && e.keyCode<=123) /*F1-F12*/ 
@@ -24,4 +24,11 @@ $(window).keydown(function(e){
 		*/
 		return false;
 	}
+	if(e.keyCode == 107 /*Plus Key*/){
+		var evt = jQuery.Event("keydown",{keyCode:9});
+		console.log(sender);
+		$(this).trigger(evt);
+		return false;
+	}
+	$('#msg_area').text("keyCode[" + e.keyCode + "] が押されました。");
 });

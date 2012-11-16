0cb936497c52a510c30b66c1a4b9433b622bd286
naohta <naohta@gmail.com>
2012-11-16 10:06:26 +0900

    anyway

---
+++ WebContent/js2/key.js
@@ -0,0 +1,45 @@
+/*
+ * Allow backspace key 
+ */
+$('input').keydown(function(e){
+	console.log("keyCode[" + e.keyCode + "] detected by $(':password, :text, textarea').");
+	if(e.keyCode==8){
+	  e.stopPropagation();
+	  return true;
+	}
+});
+
+
+
+
+
+/*
+ * Prevent backspace key & function keys (F1-F12)
+ */
+$(window).keydown(function(e){
+	console.log("keyCode[" + e.keyCode + "] detected by $(window).");
+	
+	if(		
+			e.keyCode == 8 /*backspace*/
+			|| (112<=e.keyCode && e.keyCode<=123) /*F1-F12*/ 
+	){
+		console.log("keyCode[" + e.keyCode + "] is prevented.");
+		return false;
+	}
+});
+
+
+
+/*
+ * ======================================================================
+ * Allow direct alphabet
+ */
+/*
+$('.direct_alphabet').keydown(function(e){ /*donot include #main_content
+	if(65 <= e.keyCode && e.keyCode==90){
+	  $(this).val($(this).val() + String.fromCharCode(e.keyCode+32));
+	  e.stopPropagation();
+	  return true;
+	}
+});
+*/

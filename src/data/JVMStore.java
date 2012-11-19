af4b5891b07a0a617f291c3fc68a6dde673c1490
naohta <naohta@gmail.com>
2012-11-19 14:44:12 +0900

    LinkedHashMap

--- src/data/JVMStore.java
+++ src/data/JVMStore.java
@@ -6,7 +6,7 @@ import net.arnx.jsonic.*;
 
 public class JVMStore {
 	protected static SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd HH:mm (ss.SSS)");
-    public static Map map = new HashMap();
+    public static Map map = new LinkedHashMap();
     protected static JSON json = new JSON();
     
 	@JSONHint(ordinal=0) public String uuid;

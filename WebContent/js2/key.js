28d0ecb64bba41c4d31a533d005895caa2287ad2
naohta <naohta@gmail.com>
2012-11-29 11:39:04 +0900

    js2/key.js WORK!! Tabstops handling! Yay!

--- WebContent/js2/key.js
+++ WebContent/js2/key.js
@@ -1,22 +1,67 @@
-/*
- * Prevent backspace key & function keys (F1-F12)
- */
-$(document).keydown(function(e){
-	var type = e.target.type;
-	if(type == 'text'){return true;}
-	if(type == 'password'){return true;}
-	if(type == 'textarea'){return true;}
+var key = function(){
+	// Prevent F1 Help on IE
+	window.onhelp = function(){return false;};
 	
-	var code = e.keyCode;
-	if(code==8 /*backspace*/ || (112<=code && code<=123)/*F1-F12*/){
-		alert(code);
+	var prevent = function(code){
 		console.log("keyCode[" + code + "] is prevented.");
 		if(document.all/*IE*/){window.event.keyCode = 0;}
 		return false;
-	}
-});
-
-/*
- * Prevent F1 Help on IE
- */
-window.onhelp=function(){return false;};
+	};
+	
+	var isInputType = function(type){
+		if(type == 'text') return true;
+		if(type == 'password') return true;
+		if(type == 'textarea') return true;
+		if(type == 'select') return true;
+		return false;
+	};
+	
+	var tabstops = []; 
+	
+	var moveToNextTabStop = function(){
+		
+	};
+	
+	$(document).keydown(function(e){
+		var type = e.target.type;
+		var code = e.keyCode;
+		switch(code){
+			case 8: /*backspace*/
+				if(isInputType(type)) return true;
+				return prevent(code);
+			case 112: /*F1*/
+				alert("F1 key down!");
+				return prevent(code);
+			case 13: /*Enter*/
+				if(type == 'textarea') return true;
+				/* GOTO case 9 */
+			case 9: /*Tab*/
+				var lastIdx = tabstops.length - 1;
+				$(tabstops).each(function(idx){
+					if(this[0] == e.target){
+						if(idx == lastIdx){
+							tabstops[0][0].focus();
+						}else{
+							tabstops[idx+1][0].focus();
+						}
+						return false; // break for jQuery's each().
+					}
+				});
+				return prevent(code);
+		}
+	});
+	
+	return { 
+		registTabStopArea: function(id){ // Public function
+			tabstops = [];
+			$('#'+id).find("input,textarea").each(function(){
+				console.log($(this));
+				/*if(isInputType($(this).type)) */tabstops.push($(this));
+			});
+			console.log("key->");
+			console.log(tabstops);
+			console.log("<-key");
+		}
+	};
+	
+}();

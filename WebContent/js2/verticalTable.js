591dbbb279df6de61c062ca65c4d9a917ee1dfa8
naohta <naohta@gmail.com>
2012-11-30 16:11:58 +0900

    verticalTable.js refactored!!!!!! ya!!

--- WebContent/js2/verticalTable.js
+++ WebContent/js2/verticalTable.js
@@ -7,8 +7,9 @@ var verticalTable = function(){
 	// --------------------------------------------
 	// Private members
 	// --------------------------------------------
-	var td_head = function(data){return "<td>" + data + "</td>";};
-	var td = function(data){return "<td align='left' style='width:100%;word-break:break-all;'>" + data + "</td>";};
+	var tr = function(data){return "<tr>" + data + "</tr>";};
+	var td1 = function(data){return "<td>" + data + "</td>";};
+	var td2 = function(data){return "<td align='left' style='width:100%;word-break:break-all;'>" + data + "</td>";};
 
 	var dig = function(s/*string*/,o/*object*/,parentkey){
 		// dig function 's private function.
@@ -18,25 +19,12 @@ var verticalTable = function(){
 		// dig function 's  main procedure.
 		for(var key in o){
 			if(key=='resultInJSON'){console.log("resultInJSON !!");continue;} // TODO delete this line.
-			var value = o[key];
-			var valueType = typeof(value);
-			if(valueType == 'object'){
-				var k;
-				if(parentkey==null){
-					k = key;
-				}else{
-					k = parentkey + "." + key;
-				}
-				s = dig(s,value,k); /*Recursive call*/
-			}else{
-				var keyForDisp;
-				if(parentkey==null){
-					keyForDisp = key;
-				}else{
-					keyForDisp = parentkey + "." + key;
-				}
-				s += li( spanForKey(keyForDisp) + spanForValue(value) );
-			}
+			var val = o[key];
+			var k = (parentkey) ? parentkey+"."+key : key;
+			if(typeof(val)=='object')
+				s = dig(s,val,k);
+			else
+				s += li(spanForKey(k)+spanForValue(val));
 		}
 		return s;
 	};
@@ -48,7 +36,7 @@ var verticalTable = function(){
 		build: function($tbl, records){
 			$tbl.empty();
 			for(var i=0; i<records.length; i++){
-				$tbl.append("<tr>" + td_head(i+1) + td(dig("",records[i])) + "</tr>");
+				$tbl.append(tr(td1(i+1)+td2(dig("",records[i]))));
 			}
 		}
 	};

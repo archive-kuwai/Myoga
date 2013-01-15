a93f0e9c90bce317d163aaf3acd3b32d2c5cf462
naohta <naohta@gmail.com>
2013-01-15 11:22:15 +0900

    merge Myogoin!

--- src/webapi/command/Command.java
+++ src/webapi/command/Command.java
@@ -1,13 +1,7 @@
 package webapi.command;
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.*;
-
-import org.codehaus.jackson.annotate.JsonIgnore;
-
 import data.JVMStore;
-import net.arnx.jsonic.JSON;
 import net.arnx.jsonic.JSONHint;
 
 public class Command extends JVMStore{
@@ -18,6 +12,4 @@ public class Command extends JVMStore{
 	@JSONHint(ordinal=1) public Date srvIn;
 	@JSONHint(ordinal=2) public Date srvOut;
 	@JSONHint(ordinal=3) public Method method;
-
-	
 }
